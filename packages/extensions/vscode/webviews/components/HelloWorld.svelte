c7cf71439f844f7a0f9668a08db8b60866045281
Kobe Fung <kobefungs@gmail.com>
2024-07-04 07:50:43 +0700

    added client form tabs in helloworld panel

--- packages/extensions/vscode/webviews/components/HelloWorld.svelte
+++ packages/extensions/vscode/webviews/components/HelloWorld.svelte
@@ -1,9 +1,11 @@
 <script>
   import Header from './body/Header.svelte';
   import MainContent from './body/MainContent.svelte';
+  import ClientFormTab from './body/ClientFormTab.svelte';
 </script>
 
 <div class="h-screen flex flex-col">
   <Header />
   <MainContent class="flex-grow" />
+  <ClientFormTab />
 </div>
