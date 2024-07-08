0cf6202381be5f4a8e259a69d2f135147452b26d
Adam D. <adam@adamduda.com>
2024-07-07 23:09:18 -0400

    refactor:  forgot the dropdown buttonf or dynamic selection of filters

---
+++ packages/extensions/vscode/webviews/components/asset-library/dropdown/FilterDropdown.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { filterCriteria } from '../../../stores/fileMgmt/viewDerivedStore';
+  import { filterManager } from '../../../stores/fileMgmt/viewDerivedStore/viewFiltersRouters';
+  
+  let selectedFilter = filterManager.availableFilters[0];
+
+  function handleFilterChange(event) {
+    filterCriteria.set(event.target.value);
+  }
+</script>
+
+<select bind:value={selectedFilter} on:change={handleFilterChange}>
+  {#each filterManager.availableFilters as filter}
+    <option value={filter}>{filter}</option>
+  {/each}
+</select>
